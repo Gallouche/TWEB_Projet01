544fd49f98cedf9ddc23c4671506cf59c9d90e16
Théo Gallandat <theo.gallandat@heig-vd.ch>
2018-10-05 15:14:56 +0200

    starting express server

--- Server/app.js
+++ Server/app.js
@@ -2,15 +2,24 @@
 require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
+const Github = require('./src/Github');
+// const utils = require('./src/utils');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const client = new Github({ token: process.env.OAUTH_TOKEN });
 
 // Enable CORS for the client app
 app.use(cors());
 
 app.get('/', (req, res, next) => {
-  res.send('Hello World!');
+  res.send({ message: 'hi there' });
+});
+
+app.get('/:repo', (req, res, next) => { // eslint-disable-line no-unused-vars
+  client.getRepo(req.params.repo)
+    .then(repo => res.send(repo))
+    .catch(next);
 });
 
 // Forward 404 to error handler
